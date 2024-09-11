62c63e9e4f31a0749969c842c56671f228ecb3d3
Patrick M. De Luca <patrick_mdl@outlook.com>
2024-09-11 14:47:26 -0300

    ST-18 -> Endpoints da Habilidade de classe finalizados. Customização do Swagger.

---
+++ StoryTeller/StoryTeller/Controllers/ClassAbilityController.cs
@@ -0,0 +1,119 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StoryTeller.AppDataContext;
+using StoryTeller.Interface;
+using StoryTeller.Models;
+using StoryTeller.Services;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace StoryTeller.Controller
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ClassAbilityController : ControllerBase
+    {
+        private readonly IClassAbilityServices _classAbilityServices;
+
+        public ClassAbilityController(IClassAbilityServices classAbilityServices)
+        {
+            _classAbilityServices = classAbilityServices;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Buscar todas as habilidades de classe", Description = "Retorna todas as habilidades de classe cadastradas no sistema.")]
+        public async Task<IActionResult> GetAllAbilityClasses()
+        {
+            try
+            {
+                var abilitiesMap = await _classAbilityServices.GetAllAbilities();
+                if (abilitiesMap == null || !abilitiesMap.Any())
+                    return NotFound(new { message = "Nenhuma habilidade de classe encontrada." });
+                return Ok(new { message = "Habilidades de classe encontradas.", data = abilitiesMap });
+            }
+            catch (Exception err)
+            {
+                return StatusCode(
+                    500,
+                    new { message = "Erro ao bustar todas as habilidades de classe.", err }
+                    );
+            }
+        }
+
+        [HttpGet("{Id}")]
+        [SwaggerOperation(Summary = "Buscar habilidade de classe por ID", Description = "Retorna uma habilidade de classe baseado no ID dela.")]
+        public async Task<IActionResult> GetAbilityClassById(int Id)
+        {
+            try
+            {
+                var userMap = await _classAbilityServices.GetClassAbilityById(Id);
+                if (userMap == null)
+                    return NotFound("Habilidade de Classe não encontrada.");
+                return Ok(new { message = "Habilidade de classe encontrada com sucesso.", data = userMap });
+            }
+            catch (Exception err)
+            {
+                return StatusCode(
+                    500,
+                    new { message = $"Erro ao buscar a classe de Id {Id}", err }
+                    );
+            }
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Summary = "Criar uma habilidades de classe", Description = "Insere uma nova habilidade de classe no banco")]
+        public async Task<IActionResult> CreateAbilityClass(ClassAbility classAbility)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+                await _classAbilityServices.CreateClassAbility(classAbility);
+                return StatusCode(203, new {message = "Habilidade de classe criada com sucesso"});
+            }
+            catch (Exception err)
+            {
+                return StatusCode(
+                    500,
+                    new { message = "Erro ao criar habilidade de classe.", err });
+            }
+        }
+
+        [HttpPut("{Id}")]
+        [SwaggerOperation(Summary = "Atualizar uma habilidade de classe", Description = "Atualiza as informações de uma habilidade de classe.")]
+        public async Task<IActionResult> UpdateAbilityClass(int Id, ClassAbilityDTO classAbility)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+                await _classAbilityServices.UpdateClassAbility(Id, classAbility);
+                return NoContent();
+            }
+            catch (Exception err)
+            {
+                return StatusCode(
+                    500,
+                    new { message = "Erro ao atualizar habilidade de classe.", err });
+            }
+        }
+
+        [HttpDelete("{Id}")]
+        [SwaggerOperation(Summary = "Remove as habilidades de classe", Description = "Remove a habilidade de classe pelo ID inserido.")]
+        public async Task<IActionResult> DeleteAbilityClass(int Id)
+        {
+            try
+            {
+                await _classAbilityServices.DeleteClassAbility(Id);
+                return NoContent();
+            }
+            catch (Exception err)
+            {
+                return StatusCode(
+                    500,
+                    new { message = "Erro ao remover habilidade de classe." });
+            }
+        }
+
+    }
+}
--- StoryTeller/StoryTeller/Program.cs
+++ StoryTeller/StoryTeller/Program.cs
@@ -1,35 +1,73 @@
+using Microsoft.EntityFrameworkCore;
 using StoryTeller.AppDataContext;
 using StoryTeller.Interface;
 using StoryTeller.Models;
 using StoryTeller.Services;
 using StoryTellerAPI.Middleware;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using StoryTeller.SwaggerSchemaFilter;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Adiciona os serviços necessários à aplicação
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configura o Swagger com personalizações
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "StoryTeller API",
+        Version = "v1",
+        Description = "API que alimentará o StoryTeller, um site para jogar RPG de mesa usando o sistema Ordem Paranormal.",
+        Contact = new OpenApiContact
+        {
+            Name = "Patrick Medeiros De Luca",
+            Email = "patrick_mdl@outlook.com.com",
+            Url = new Uri("https://github.com/patrickMDL")
+        }
+    });
+    c.EnableAnnotations();
+    c.SchemaFilter<UserSchemaFilter>();
+    c.SchemaFilter<ClassAbilitySchemaFilter>();
+    // Incluir comentários XML para gerar documentação automática
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+// Configurações do banco de dados e outras dependências
 builder.Services.Configure<DbSettings>(builder.Configuration.GetSection("DbSettings"));
-builder.Services.AddSingleton<StorytellerDbContext>();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-
-builder.Services.AddProblemDetails();  
+builder.Services.AddDbContext<StorytellerDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString")));
+builder.Services.AddProblemDetails();
 
 builder.Services.AddLogging();
 builder.Services.AddScoped<IUserServices, UserServices>();
+
 var app = builder.Build();
+
+// Criação de escopo para serviços
+using (var scope = app.Services.CreateScope())
 {
-    using var scope = app.Services.CreateScope();
     var context = scope.ServiceProvider;
+    // Possível uso do contexto ou inicialização de serviços
 }
+
+// Configuração do middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "StoryTeller API v1");
+        c.RoutePrefix = string.Empty;  // Exibe o Swagger na raiz da aplicação
+    });
 }
 
 app.UseHttpsRedirection();
